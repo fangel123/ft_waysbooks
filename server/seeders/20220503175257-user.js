649a784957de80a158984552f4ed0d09b71f57c4
Mikha Januardi <mikhajuntaq@gmail.com>
2022-06-04 04:44:20 +0700

    Final

---
+++ server/seeders/20220503175257-user.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          email: "admin1@mail.com",
+          password:
+            "$2a$10$K4uVVYceseuCsDO/qp0XKuCQRORl2d015heQXkVorVEkl8zh5WzEW", //admin123
+          name: "admin 1",
+          role: "admin",
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
